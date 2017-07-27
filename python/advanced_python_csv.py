718960f3a2c1f82ec226723d5d67c9e5c1812b0a
jramirezor.91@gmail.com <jramirezor.91@gmail.com>
2017-07-27 16:26:54 +0100

    Adding response to advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+import csv
+import advanced_python_regex
+
+
+with open("emails.csv", "w", newline = '' ) as f:
+    writer = csv.writer(f, delimiter = ',')
+    writer.writerows(advanced_python_regex.emails_data)
